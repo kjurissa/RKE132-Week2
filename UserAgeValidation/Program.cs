bbcae07479c2a6e8109b7c7b13a156184e1df60a
Kärt Kristina <kjurissa@tktk.ee>
2023-04-26 15:50:30 +0300

    add week 2

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//küsib vanust
+
+Console.WriteLine("Sisesta oma vanus: ");
+
+int userAge = Int32.Parse(Console.ReadLine()); //"13" - heap, 13 - stack
+
+if (userAge >= 13)
+{
+    Console.WriteLine("Tere tulemast Instagrami!");
+}
+else
+{
+    Console.WriteLine("Sa oled liiga noor Instagrami kasutamiseks!");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus küsib sugu
+//küsib perekonnanime
+//rakendus tervitab kasutajat
+//
+
+Console.WriteLine("Vali oma sugu (m/n): ");
+
+char userGender = Char.Parse(Console.ReadLine()); // loeb konsoolist string ehk sõne
+Console.WriteLine("Palun sisesta oma perekonnanimi: ");
+string userLastName = Console.ReadLine();
+
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Tere, Mr.{userLastName}");
+}
+else if (userGender == 'n')
+{
+    Console.WriteLine($"Tere, Ms.{userLastName}");
+}
+else
+{
+    Console.WriteLine($"Tere {userLastName}");
+}
+
+
+//Console.WriteLine($"Tere, {userGender}");
